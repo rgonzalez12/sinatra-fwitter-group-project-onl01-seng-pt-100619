0b9ed4040020ea010efa78c650b0fa0d1fd4664a
Raymond Gonzalez <raymondalexander.gonzalez@gmail.com>
2020-02-05 03:58:11 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,37 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "get_memed"
   end
+  
+  get '/' do
+    erb :index
+  end
+  
+  helpers do
 
+   def logged_in?
+     !!current_user
+   end
+
+   def current_user
+     @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+   end
+   
+   def login(username, password)
+    user = User.find_by(:username => username) 
+     if user && user.authenticate(password)
+      session[:user_id] = user.id
+     else
+      redirect "/login"
+     end
+   end
+    
+   def logout!
+    session.clear
+   end
+
+  end
+  
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,50 @@
 class UsersController < ApplicationController
+  
+  get '/users/:slug' do
+   @user = User.find_by_slug(params[:slug])
+   erb :'users/show'
+  end
+  
+  get '/signup' do
+   if !logged_in?
+      erb :'/users/create_user'
+   else
+      redirect to '/tweets'
+   end
+  end
 
+  post '/signup' do
+    if params[:username] == "" || params[:email] == "" || params[:password] == ""
+      redirect to '/signup'
+    else
+      @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+      @user.save
+      session[:user_id] = @user.id
+      redirect to '/tweets'
+    end
+  end
+  
+  get '/login' do
+    if !logged_in?
+     erb :'/users/login'
+    else
+     redirect to '/tweets/tweets'
+    end
+  end
+  
+  post '/login' do
+    login(params[:username], params[:password])
+    current_user
+    redirect to '/tweets'
+  end
+  
+  get '/logout' do
+    if logged_in?
+     logout!
+     redirect to '/login'
+    else
+     redirect to '/'
+    end
+  end
 
 end
